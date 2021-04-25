5b90c81f4d060b81a64de337def2ab3b2b03129f
Kaitlin Rockway <krockway@gmail.com>
2021-04-24 22:08:31 -0700

    Add multiple points to a map

--- Mapping_Multiple_Points/static/js/logic.js
+++ Mapping_Multiple_Points/static/js/logic.js
@@ -4,26 +4,6 @@ console.log("working");
 // Create the map object with US center and zoom level.
 let map = L.map('mapid').setView([40.7, -94.5], 4);
 
-// // Add marker to the map for LA, CA
-var marker = L.marker([34.0522, -118.2437]).addTo(map);
-
-// // Add 300 METER radius circle to the map for LA, CA
-// var circle = L.circle([34.0522, -118.2437], {
-//     radius: 300,
-//     color: "#000000",
-//     fillColor: "#ffff00",
-//     fillOpacity: .2
-// }).addTo(map);
-
-// Add 300 PIXEL radius circle to the map for LA, CA
-//Note: the size of the circle will change as you zoom
-// var circleMarker = L.circleMarker([34.0522, -118.2437], {
-//     radius: 300,
-//     color: "black",
-//     fillColor: "#ffff00",
-//     fillOpacity: .2
-// }).addTo(map);
-
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
@@ -37,3 +17,26 @@ let streets = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
 
+// Get data from cities.js
+let cityData = cities;
+
+// // Loop through the cities array and create one marker for each city.
+// cityData.forEach(function(city) {
+//   console.log(city)
+//   L.marker(city.location)
+//   .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
+//   .addTo(map);
+//  });
+
+// Loop through the cities array and create one circle for each city.
+cityData.forEach(function(city) {
+  console.log(city)
+  L.circleMarker(city.location, {
+    radius: city.population/200000,
+    color: "orange",
+    fillColor: "orange",
+    // fillOpacity: .2
+  })
+  .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
+  .addTo(map);
+ });
