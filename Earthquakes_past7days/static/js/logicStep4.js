9f17f47076d93e147dd76b12083c9e3a50a8697a
Kaitlin Rockway <krockway@gmail.com>
2021-04-27 23:25:47 -0700

    Earthwuakes added as layer

--- Earthquakes_past7days/static/js/logicStep4.js
+++ Earthquakes_past7days/static/js/logicStep4.js
@@ -1,6 +1,9 @@
 // Add console.log to check to see if our code is working.
 console.log("working");
 
+// Retrieve the earthquake GeoJSON data.
+let earthquake7day = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
@@ -21,24 +24,29 @@ let satelliteStreets = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/
     accessToken: API_KEY
   });
 
+// Create the map object with US center, zoom level and default layer.
+let map = L.map('mapid', {
+  center: [39.5, -98.5],
+  zoom: 3,
+  layers: [streets]
+});  
+
 // Create a base layer that holds both maps.
 let baseMaps = {
   "Streets": streets,
   "Satellite": satelliteStreets
 };
 
-// Create the map object with US center, zoom level and default layer.
-let map = L.map('mapid', {
-  center: [39.5, -98.5],
-  zoom: 3,
-  layers: [streets]
-});
+// Create the earthquake layer for our map (has to be below baseMaps)
+let earthquakes = new L.layerGroup();
 
-// Pass our map layers into our layers control and add the layers control to the map.
-L.control.layers(baseMaps).addTo(map);
+// We define an object that contains the overlays. This overlay will be visible all the time.
+let overlays = {
+  "Earthquakes": earthquakes
+};
 
-// Retrieve the earthquake GeoJSON data.
-let earthquake7day = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+// Then we add a control to the map that will allow the user to change which layers are visible.
+L.control.layers(baseMaps, overlays).addTo(map);
 
 // Create a style for the lines.
 let myStyle = {
@@ -46,9 +54,6 @@ let myStyle = {
   color: "blue", //stroke color
   weight: 1
 }
-
-
-
 
 // Grabbing our GeoJSON data & styling as element
 d3.json(earthquake7day).then(function(data) {
@@ -105,7 +110,10 @@ d3.json(earthquake7day).then(function(data) {
     onEachFeature: function(feature, layer) {
       layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place);
     }
-  }).addTo(map);
+  }).addTo(earthquakes);
+
+  //Then add earthquake layer to our map
+  earthquakes.addTo(map);
 });
 
 
